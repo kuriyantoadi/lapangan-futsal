9db73c486d5edb189732d4211397f102df1abb1b
kuriyantoadi <melatikuning212@gmail.com>
2021-12-24 23:21:32 +0700

    update 24 desember 2021, update 5

--- application/controllers/Daftar.php
+++ application/controllers/Daftar.php
@@ -7,7 +7,7 @@ class Daftar extends CI_Controller {
 	public function index()
 	{
     $this->load->view('template/header');
-		$this->load->view('user/daftar');
+		$this->load->view('pelanggan/daftar');
     $this->load->view('template/footer');
 
 	}
